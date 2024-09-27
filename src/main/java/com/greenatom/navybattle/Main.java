5840207b875ad23ddda3cf7bd10baf0b87c09298
TimurTimergalin <timur20041229@gmail.com>
2024-09-27 18:26:42 +0300

    Создан ui для размещения кораблей на поле

--- src/main/java/com/greenatom/navybattle/Main.java
+++ src/main/java/com/greenatom/navybattle/Main.java
@@ -1,34 +1,11 @@
 package com.greenatom.navybattle;
 
-import com.greenatom.navybattle.view.components.Chat;
-import com.greenatom.navybattle.view.components.InputLine;
-import com.greenatom.navybattle.view.components.TextLine;
-import com.greenatom.navybattle.view.components.field.Field;
-import com.greenatom.navybattle.view.components.field.TileStatus;
-import com.greenatom.navybattle.view.utils.Printer;
+import com.greenatom.navybattle.view.ShipPlacementView;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        new Printer().clearAll().flush();
-        var field = new Field(4, 4)
-                .draw()
-                .changeStatus(1, 1, TileStatus.HORIZONTAL)
-                .changeStatus(2, 1, TileStatus.HORIZONTAL)
-                .changeStatus(3, 1, TileStatus.HIT)
-                .changeStatus(5, 8, TileStatus.MISS)
-                .changeStatus(7, 3, TileStatus.VERTICAL)
-                .changeStatus(7, 4, TileStatus.VERTICAL)
-                .changeStatus(7, 5, TileStatus.VERTICAL)
-                .changeStatus(7, 6, TileStatus.VERTICAL);
-
-        var chat = new Chat(field.getRight() + 3, field.getTop() + 1, 4);
-
-        var inputLabel = new TextLine(field.getRight() + 3, chat.getBottom() + 2, "Type something: ");
-        var input = new InputLine(inputLabel.getRight() + 1, inputLabel.getTop());
-        for (int i = 0; i < 5; ++i) {
-            chat.addMessage(input.getInput());
-        }
-
-        new Printer().goTo(20, 1).flush();
+        new ShipPlacementView(10, List.of(1, 2, 3, 4)).draw();
     }
 }
---
+++ src/main/java/com/greenatom/navybattle/view/ShipPlacementView.java
@@ -0,0 +1,88 @@
+package com.greenatom.navybattle.view;
+
+import com.greenatom.navybattle.view.components.InputLine;
+import com.greenatom.navybattle.view.components.TextLine;
+import com.greenatom.navybattle.view.components.field.Field;
+import com.greenatom.navybattle.view.utils.Printer;
+
+import java.util.List;
+
+public class ShipPlacementView {
+    private Field battlefield;
+    private InputLine inputLine;
+    private TextLine errorMessageLine;
+
+    private final int size;
+    private final List<Integer> allowedShips;
+
+    public ShipPlacementView(int size, List<Integer> allowedShips) {
+        this.size = size;
+        this.allowedShips = allowedShips;
+    }
+
+    public void draw() {
+        new Printer().toggleCursor(false).clearAll();
+        battlefield = new Field(2, 2);
+        battlefield.draw();
+
+        var inputLabel = new TextLine(battlefield.getLeft(), battlefield.getBottom() + 2, "Your command: ");
+        inputLine = new InputLine(inputLabel.getRight() + 1, inputLabel.getTop());
+
+        errorMessageLine = new TextLine(inputLabel.getLeft(), inputLabel.getBottom() + 1);
+        drawHints();
+        putAtEnd();
+    }
+
+    private void drawHints() {
+        var lastHint = new TextLine(
+                battlefield.getRight() + 4,
+                battlefield.getTop() + 1,
+                "You may use the following commands:"
+        );
+
+        lastHint = new TextLine(
+                lastHint.getLeft(),
+                lastHint.getBottom() + 2,
+                "put (" +
+                        String.join("|", allowedShips.stream().map(Object::toString).toList()) +
+                        ") [A-" +
+                        (char) ('A' + size - 1) +
+                        "][1-" +
+                        size +
+                        "] (U|D|L|R)"
+        );
+
+        lastHint = new TextLine(
+                lastHint.getLeft(),
+                lastHint.getBottom() + 2,
+                "undo"
+        );
+
+        lastHint = new TextLine(
+                lastHint.getLeft(),
+                lastHint.getBottom() + 2,
+                "start"
+        );
+
+        new TextLine(
+                lastHint.getLeft(),
+                lastHint.getBottom() + 2,
+                "exit"
+        );
+    }
+
+    public String getCommand() {
+        String inp = inputLine.getInput();
+        putAtEnd();
+        return inp;
+    }
+
+    public void setErrorMessage(String message) {
+        errorMessageLine.setText(message);
+        putAtEnd();
+    }
+
+    private void putAtEnd() {
+        new Printer().goTo(errorMessageLine.getBottom() + 1, 1);
+    }
+}
